592bfdffb63f6923c809c02f40caa6b65c35f2ba
MohdAkhtar <mohammedakhtar01@gmail.com>
2024-09-15 12:24:59 +0530

    fail error validation test

--- src/test/java/rahulshettyacademy/tests/ErrorValidationTest.java
+++ src/test/java/rahulshettyacademy/tests/ErrorValidationTest.java
@@ -33,7 +33,7 @@ public class ErrorValidationTest extends BaseTest{
 		// TODO Auto-generated method stub
 		
 		
-		loginPage.loginToApplication("abcde@gmail.com", "Udemy@123");
+		loginPage.loginToApplication("mohdakhtar@gmail.com", "Udemy@123");
 		
 		System.out.println(loginPage.getErrorMsg());
 		
